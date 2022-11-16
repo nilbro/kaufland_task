dbbf1bf2bd2db39a52b55fab90e3ca6d6daefcb5
Patra, Nilhabra [CBC] <nilabhra.patra@cbc.de>
2022-11-16 07:12:26 +0100

    OOP

--- simple_consumer.py
+++ simple_consumer.py
@@ -1,27 +1,27 @@
 from kafka import KafkaConsumer, KafkaProducer
 
 
-def simple_consumer():
-    c = KafkaConsumer("data-input", bootstrap_servers="localhost:9092")
+class Consumer():
+    def __init__(self, id) -> None:
+        self.c = KafkaConsumer("data-input", bootstrap_servers="localhost:9092", group_id=id)
 
-    local_cache = []
+    def consume_message(self):
+        self.local_cache = []
 
-    while True:
-        msg_pack = c.poll(timeout_ms=50000)
-        for messages in msg_pack.items():
-            for message in messages:
-                print(message.value.decode('utf-8'))
-                local_cache.append(message.value.decode('utf-8'))
-        local_cache.sort()
-        send_messages(local_cache)
+        while True:
+            msg_pack = self.c.poll(timeout_ms=50000)
+            for tp, messages in msg_pack.items():
+                for message in messages:
+                    self.local_cache.append(message.value.decode("utf-8"))
+            self.local_cache.sort()
+            self.send_messages()
 
-
-def send_messages(sorted_list: list):
-    producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
-    for msg in sorted_list:
-        producer.send('data-output', msg.encode("utf-8"))
+    def send_messages(self):
+        producer = KafkaProducer(bootstrap_servers=["localhost:9092"])
+        for msg in self.local_cache:
+            producer.send("data-output", msg.encode("utf-8"))
 
 
 if __name__ == "__main__":
-    simple_consumer()
-
+    c = Consumer('my-group')
+    c.consume_message()
